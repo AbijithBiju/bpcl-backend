d98d45d5e1484101d893d32042da78e9d8e7a931
AbijithBiju <abijith001971@gmail.com>
2023-03-08 16:50:28 +0530

    add jwt expiration option

--- api/User.js
+++ api/User.js
@@ -1,63 +1,59 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const jwt = require('jsonwebtoken')
-const SECRET_KEY = process.env.JWT_SECRET_KEY || 'my_password'
-const tokenDuration = 60*60*3 // 3 hours
-const verifyToken = require('../middleware/verifyToken')
+const jwt = require("jsonwebtoken");
+const SECRET_KEY = process.env.JWT_SECRET_KEY || "my_password";
+const tokenDuration = 60 * 60 * 3; // 3 hours
+const verifyToken = require("../middleware/verifyToken");
 
 const user = {
-    userName:'user',
-    password:'12345678',
-    userId  :'user123',
-    type    :'normal'
-}
+    userName: "user",
+    password: "12345678",
+    userId: "user123",
+    type: "normal",
+};
 
-router.post('/login',(req,res)=>{
-    console.log(req.body)
-    const {userName,password} = req.body
-    if( user.userName===userName && user.password===password){
+router.post("/login", (req, res) => {
+    console.log(req.body);
+    const { userName, password } = req.body;
+    if (user.userName === userName && user.password === password) {
+        console.log("login success");
 
-        console.log('login success')
+        const accessToken = jwt.sign(
+            {
+                userName: user.userName,
+                userType: user.type,
+                userId: user.userId,
+            },
+            SECRET_KEY,
+            {
+                expiresIn: "3h",
+            }
+        );
 
-        const accessToken = jwt.sign({
-            userName:user.userName,
-            userType:user.type,
-            userId:user.userId,
-        },SECRET_KEY)
-
-        console.log({accessToken})
+        console.log({ accessToken });
 
         res.json({
-            status:'SUCCESS',
-            userId:user.userId,
-            token :accessToken
-        })
-
-    }else{
-        res.status(403)
+            status: "SUCCESS",
+            userId: user.userId,
+            userType: user.type,
+            token: accessToken,
+        });
+    } else {
+        res.status(403);
         res.json({
-            status:'FAILED'
-        })
+            status: "FAILED",
+        });
     }
-})
+});
 
-router.get('/:userName',verifyToken,(req,res)=>{
-    if(req.tokenData.iat+tokenDuration > Math.trunc(Date.now()/1000)){
-        if(req.tokenData.userType === 'normal'){
-            res.json({
-                token:'not expired',
-                userType:'normal',
-                data : [1,2,3,4,5,6]
-            })
-        }
-    }else{
+router.get("/:userName", verifyToken, (req, res) => {
+    if (req.tokenData.userType === "normal") {
         res.json({
-            token:'expired'
-        })
+            userType: "normal",
+            data: [1, 2, 3, 4, 5, 6],
+        });
     }
-})
+});
 
-
-
-module.exports = router
+module.exports = router;
